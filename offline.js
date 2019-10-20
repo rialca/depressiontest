b2df6faa3c83213be7a64641fa08f76466ad071b
rialca <49744506+rialca@users.noreply.github.com>
2019-10-20 14:55:53 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1571557591,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/abutton-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/checkbox-sheet0.png",
+		"images/checkbox-sheet1.png",
+		"images/untitled-sheet0.png",
+		"images/bcal-sheet0.png",
+		"images/scroll-sheet0.png",
+		"images/bdown-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/scorepopup-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
